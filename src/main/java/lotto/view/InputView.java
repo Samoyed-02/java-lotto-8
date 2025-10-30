1aaaf85826e5d772e978e41d377d0566f1e38435
Samoyed-02 <gus02031365@gmail.com>
2025-10-30 20:58:27 +0900

    feat(InputView): 입력 메세지 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private final static String INPUT_AMOUNT = "구입금액을 입력해 주세요.";
+    private final static String INPUT_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
+    private final static String INPUT_BONUS_LOTTO_NUMBER ="보너스 번호를 입력해 주세요.";
+
+    public String inputAmount(){
+        System.out.println(INPUT_AMOUNT);
+        return Console.readLine();
+    }
+
+    public String inputLottoNumber(){
+        System.out.println(INPUT_LOTTO_NUMBER);
+        return Console.readLine();
+    }
+
+    public String inputBonusLottoNumber(){
+        System.out.println(INPUT_BONUS_LOTTO_NUMBER);
+        return Console.readLine();
+    }
+}
