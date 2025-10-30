e0738df1002721adfb94e3b240b6703c1d3c3846
Samoyed-02 <gus02031365@gmail.com>
2025-10-30 20:02:55 +0900

    test(AmountTest): Amount 클래스 단위 테스트 추가

--- src/test/java/lotto/domain/AmountTest.java
+++ src/test/java/lotto/domain/AmountTest.java
@@ -14,7 +14,7 @@ class AmountTest {
 
     @DisplayName("규칙에 맞는 금액이면 정상적으로 생성")
     @Test
-    void 금액_생성_테스트() {
+    void test_금액_생성_테스트() {
         int testAmount = 3000;
         Amount amount = new Amount(testAmount);
 
@@ -24,14 +24,14 @@ class AmountTest {
     @ParameterizedTest
     @DisplayName("허용되지 않은 금액이면 예외 발생 ")
     @ValueSource(ints = {0, -100})
-    void 허용안한_금액_예외_처리(int testAmount) {
+    void test_허용안한_금액_예외_처리(int testAmount) {
         assertThatThrownBy(() -> new Amount(testAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("금액 제한을 넘으면 예외 발생")
-    void 금액_제한_예외_처리() {
+    void test_금액_제한_예외_처리() {
         int testAmount = 101000;
         assertThatThrownBy(() -> new Amount(testAmount))
                 .isInstanceOf(IllegalArgumentException.class);
