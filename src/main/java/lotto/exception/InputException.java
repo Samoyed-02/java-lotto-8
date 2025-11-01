965d53fd57e7fe1ca7885d50129e0417cb35ff51
Samoyed-02 <gus02031365@gmail.com>
2025-11-01 16:27:44 +0900

    feat(InputException): Lotto 번호와 통합 입력에 대한 예외 메세지 추가

--- src/main/java/lotto/exception/InputException.java
+++ src/main/java/lotto/exception/InputException.java
@@ -2,11 +2,18 @@
 package lotto.exception;
 
 public enum InputException {
-    AMOUNT_INDIVISIBLE("금액이 1000원의 배수가 아닙니다."),
-    AMOUNT_OVER("최대 금액을 초과했습니다.(최대 100,000원)"),
-    AMOUNT_ZERO_MINUS("금액이 0원이거나 잘못되었습니다.");
+    AMOUNT_INDIVISIBLE("[ERROR] 금액이 1000원의 배수가 아닙니다. 다시 작성해주세요."),
+    AMOUNT_OVER("[ERROR] 최대 금액을 초과했습니다.(최대 100,000원) 다시 작성해주세요."),
+    AMOUNT_ZERO_MINUS("[ERROR] 금액이 0원이거나 잘못되었습니다. 다시 작성해주세요."),
+    LOTTO_SIZE_LESS_OR_OVER("[ERROR] 로또 번호는 6개 작성해주세요. 다시 작성해주세요."),
+    LOTTO_NUMBER_ZERO_MINUS("[ERROR] 로또 번호는 1부터 입니다. 다시 작성해주세요."),
+    LOTTO_NUM_SIZE_OVER("[ERROR] 로또 번호는 45까지 입니다. 다시 작성해주세요."),
+    LOTTO_NUM_DUPLICATION("[ERROR] 로또 번호가 중복되었습니다. 다시 작성해주세요."),
+    LOTTO_NUM_HAVE_SPACE("[ERROR] 로또 번호에 띄어쓰기가 포함되어있습니다. 다시 작성해주세요"),
+    TEXT_IS_EMPTY("[ERROR] 입력 칸이 비었습니다. 다시 작성해주세요."),
+    TEXT_IS_NO_NUMBER("[ERROR] 숫자를 입력해주세요.");
 
-    private String message;
+    private final String message;
 
     InputException(String message) {
         this.message = message;
