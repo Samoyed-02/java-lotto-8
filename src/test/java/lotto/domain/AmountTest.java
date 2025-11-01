6af35f5343240855aa7ac4fe675b0b7526c29e5b
Samoyed-02 <gus02031365@gmail.com>
2025-11-01 16:30:13 +0900

    refactor(AmountTest): Amount 클래스 리펙토링에 따른 테스트 코드 수정

--- src/test/java/lotto/domain/AmountTest.java
+++ src/test/java/lotto/domain/AmountTest.java
@@ -11,12 +11,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AmountTest {
 
-
     @DisplayName("규칙에 맞는 금액이면 정상적으로 생성")
     @Test
-    void test_금액_생성_테스트() {
+    void 금액_생성_테스트() {
         int testAmount = 3000;
-        Amount amount = new Amount(testAmount);
+        Amount amount = Amount.create(testAmount);
 
         assertThat(amount.getAmount()).isEqualTo(testAmount);
     }
@@ -24,16 +23,17 @@ class AmountTest {
     @ParameterizedTest
     @DisplayName("허용되지 않은 금액이면 예외 발생 ")
     @ValueSource(ints = {0, -100})
-    void test_허용안한_금액_예외_처리(int testAmount) {
-        assertThatThrownBy(() -> new Amount(testAmount))
+    void 허용안한_금액_예외_처리(int testAmount) {
+        assertThatThrownBy(() -> Amount.create(testAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("금액 제한을 넘으면 예외 발생")
-    void test_금액_제한_예외_처리() {
+    void 금액_제한_예외_처리() {
         int testAmount = 101000;
-        assertThatThrownBy(() -> new Amount(testAmount))
+        assertThatThrownBy(() ->Amount.create(testAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
 }
