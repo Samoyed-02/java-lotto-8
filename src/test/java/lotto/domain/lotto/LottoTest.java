58215f03034389c12bf33b79cb93d9822b21a40e
Samoyed-02 <gus02031365@gmail.com>
2025-11-03 16:53:38 +0900

    test(LottoTest) : 패키지 이동

---
+++ src/test/java/lotto/domain/lotto/LottoTest.java
@@ -0,0 +1,39 @@
+package lotto.domain.lotto;
+
+import lotto.exception.InputException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTest {
+    @Test
+    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+        assertThatThrownBy(() -> Lotto.create(List.of(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(6),
+                new LottoNumber(7))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(InputException.LOTTO_SIZE_LESS_OR_OVER.getMessage());
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> Lotto.create(List.of(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(5))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(InputException.LOTTO_NUM_DUPLICATION.getMessage());
+    }
+}
