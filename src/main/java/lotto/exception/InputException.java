0577dd54d9c6d554d67a5cb82cb93be3885fc47c
Samoyed-02 <gus02031365@gmail.com>
2025-11-03 16:52:51 +0900

    refactor(InputException) : 예외 메세지 추가 및 삭제

--- src/main/java/lotto/exception/InputException.java
+++ src/main/java/lotto/exception/InputException.java
@@ -6,12 +6,12 @@ public enum InputException {
     AMOUNT_OVER("[ERROR] 최대 금액을 초과했습니다.(최대 100,000원) 다시 작성해주세요."),
     AMOUNT_ZERO_MINUS("[ERROR] 금액이 0원이거나 잘못되었습니다. 다시 작성해주세요."),
     LOTTO_SIZE_LESS_OR_OVER("[ERROR] 로또 번호는 6개 작성해주세요. 다시 작성해주세요."),
-    LOTTO_NUMBER_ZERO_MINUS("[ERROR] 로또 번호는 1부터 입니다. 다시 작성해주세요."),
-    LOTTO_NUM_SIZE_OVER("[ERROR] 로또 번호는 45까지 입니다. 다시 작성해주세요."),
+    LOTTO_NUMBER_MIN_MAX("[ERROR] 로또 번호는 1~45사이의 수를 작성해주세요."),
     LOTTO_NUM_DUPLICATION("[ERROR] 로또 번호가 중복되었습니다. 다시 작성해주세요."),
     LOTTO_NUM_HAVE_SPACE("[ERROR] 로또 번호에 띄어쓰기가 포함되어있습니다. 다시 작성해주세요"),
     TEXT_IS_EMPTY("[ERROR] 입력 칸이 비었습니다. 다시 작성해주세요."),
-    TEXT_IS_NO_NUMBER("[ERROR] 숫자를 입력해주세요.");
+    TEXT_IS_NO_NUMBER("[ERROR] 숫자를 입력해주세요."),
+    BONUS_NUM_DUPLICATION("[ERROR] 로또 번호와 보너스 번호가 중복되었습니다. 다시 작성해주세요.");
 
     private final String message;
 
