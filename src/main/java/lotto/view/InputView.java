8cdbbafc5d23ee3cc7320dde987735cf9bac11fb
Samoyed-02 <gus02031365@gmail.com>
2025-11-03 16:36:51 +0900

    refactor(InputView) : 자바 코드  컨벤션 리펙토링

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,19 +5,19 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private final static String INPUT_AMOUNT = "구입금액을 입력해 주세요.";
     private final static String INPUT_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
-    private final static String INPUT_BONUS_LOTTO_NUMBER ="보너스 번호를 입력해 주세요.";
+    private final static String INPUT_BONUS_LOTTO_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public String inputAmount(){
+    public String inputAmount() {
         System.out.println(INPUT_AMOUNT);
         return Console.readLine();
     }
 
-    public String inputLottoNumber(){
+    public String inputLottoNumber() {
         System.out.println(INPUT_LOTTO_NUMBER);
         return Console.readLine();
     }
 
-    public String inputBonusLottoNumber(){
+    public String inputBonusLottoNumber() {
         System.out.println(INPUT_BONUS_LOTTO_NUMBER);
         return Console.readLine();
     }
