92b567a9d7185daff2a362e66a3db10e443384de
Samoyed-02 <gus02031365@gmail.com>
2025-11-03 17:14:18 +0900

    feat(Application) : 로또 게임 총 실행 코드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.AppConfig.AppConfig;
+import lotto.controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+        GameController controller = appConfig.gameController();
+        controller.startLotto();
     }
 }
