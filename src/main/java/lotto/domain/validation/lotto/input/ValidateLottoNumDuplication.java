f4d6433d495949843ef0bd1068ceb67934944332
Samoyed-02 <gus02031365@gmail.com>
2025-11-03 16:38:54 +0900

    refactor(ValidateLottoNumDuplication) :  타입 수정

--- src/main/java/lotto/domain/validation/lotto/input/ValidateLottoNumDuplication.java
+++ src/main/java/lotto/domain/validation/lotto/input/ValidateLottoNumDuplication.java
@@ -1,11 +1,12 @@
-package lotto.domain.validation.LottoValidation;
+package lotto.domain.validation.lotto.input;
 
 import java.util.List;
+import lotto.domain.lotto.LottoNumber;
 import lotto.exception.InputException;
 
 public class ValidateLottoNumDuplication implements LottoValidationRule {
     @Override
-    public void validate(List<Integer> numbers) {
+    public void validate(List<LottoNumber> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()) {
             throw new IllegalArgumentException(InputException.LOTTO_NUM_DUPLICATION.getMessage());
         }
