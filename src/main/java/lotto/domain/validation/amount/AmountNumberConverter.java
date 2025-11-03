451fa16ae2dd54645753a5361059fdb358fdd67f
Samoyed-02 <gus02031365@gmail.com>
2025-11-03 16:41:10 +0900

    refactor(AmountNumberConvert) :  리펙토링

--- src/main/java/lotto/domain/validation/amount/AmountNumberConverter.java
+++ src/main/java/lotto/domain/validation/amount/AmountNumberConverter.java
@@ -1,9 +1,9 @@
-package lotto.domain.Amount;
+package lotto.domain.validation.amount;
 
 import lotto.exception.InputException;
 
 public class AmountNumberConverter {
-    public static Integer convert(String inputView) {
+    public Integer convert(String inputView) {
         if (inputView == null || inputView.trim().isEmpty()) {
             throw new IllegalArgumentException(InputException.TEXT_IS_EMPTY.getMessage());
         }
