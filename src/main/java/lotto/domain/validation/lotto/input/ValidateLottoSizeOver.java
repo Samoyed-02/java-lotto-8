f0b2c889b963d64052771258018dfd1dd18cbb7e
Samoyed-02 <gus02031365@gmail.com>
2025-11-03 16:39:09 +0900

    refactor(ValidateLottoSizeOver) :  타입 수정

--- src/main/java/lotto/domain/validation/lotto/input/ValidateLottoSizeOver.java
+++ src/main/java/lotto/domain/validation/lotto/input/ValidateLottoSizeOver.java
@@ -1,11 +1,12 @@
-package lotto.domain.validation.LottoValidation;
+package lotto.domain.validation.lotto.input;
 
 import java.util.List;
+import lotto.domain.lotto.LottoNumber;
 import lotto.exception.InputException;
 
 public class ValidateLottoSizeOver implements LottoValidationRule {
     @Override
-    public void validate(List<Integer> numbers) {
+    public void validate(List<LottoNumber> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(InputException.LOTTO_SIZE_LESS_OR_OVER.getMessage());
         }
